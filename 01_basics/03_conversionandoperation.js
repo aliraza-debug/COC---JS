cbb47b9f403deb86574e2c05940c6481caa6ce82
Apna Naam <alirazacs007@gmail.com>
2026-04-18 11:45:34 +0500

    conversion

---
+++ 01_basics/03_conversionandoperation.js
@@ -0,0 +1,24 @@
+// 03_conversionandoperation.js
+
+let score = "33abc"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+
+
+let isLoggedIn = "ADMINO" ;
+
+let valueInboolean = Boolean(isLoggedIn)
+console.log(valueInboolean)
+console.log(typeof(valueInboolean))
+
+// 1 => true , 0 => false 
+// " " => false 
+// " admino " => true 
+
