4e6eea8e76c80121e306f8e2ddcf64984f658a13
Apna Naam <alirazacs007@gmail.com>
2026-04-18 12:22:51 +0500

    operation

--- 01_basics/03_conversionandoperation.js
+++ 01_basics/03_conversionandoperation.js
@@ -2,12 +2,12 @@
 
 let score = "33abc"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 
 
@@ -15,10 +15,37 @@ console.log(valueInNumber)
 let isLoggedIn = "ADMINO" ;
 
 let valueInboolean = Boolean(isLoggedIn)
-console.log(valueInboolean)
-console.log(typeof(valueInboolean))
+// console.log(valueInboolean)
+// console.log(typeof(valueInboolean))
 
 // 1 => true , 0 => false 
 // " " => false 
 // " admino " => true 
 
+// ********************* OPERATION ************************
+
+
+
+let value = 3 
+let negValue = -value
+// console.log(negValue) 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "ADM"
+let str2 = "INO"
+let str3 = str1 + str2 ;
+// console.log(str3) ;
+
+console.log("1" + 2 )
+console.log(1 + "2" )
+console.log(1 + 2 + "2" )
+console.log("1" + 2 + 2 )
+
+// console.log(true); // true
+//  console.log(+true); // 1
+//  console.log(true+); // error
