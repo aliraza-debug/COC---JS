67ce4954bc6242305e1122a4fbec1c1214181570
Apna Naam <alirazacs007@gmail.com>
2026-04-18 11:26:31 +0500

    variables + datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123654789;
+let accountEmail = "a@agmail.com"
+var accountPass = "78964"
+
+// prefer not to use var 
+// because of issue in block scope and functional scope
+
+console.table([accountId,accountEmail,accountPass])
+console.log(accountEmail);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ;  // treat all JS code as newer version 
+// real documentation of JS ( ts39.es ) & ( MDN ) 
+
+let name = " ADMINO "
+let age = 36 ;
+let isLoggedin = true ;
+
+//  premitive data types
+
+
+// number => 2 power 53 
+// BigInt => 
+// String => " " 
+// Boolean => true / false
+// null => standalone value  // is ki typeof ( object )  
+// undefined 
+// Symbol => uniques k liy 
+
+
+
+//object 
