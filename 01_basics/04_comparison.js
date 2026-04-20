24f212453713adce357fe209db94b8a1a52d1814
aLi-RaZa <alirazacs007@gmail.com>
2026-04-20 11:00:48 +0500

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,23 +1,23 @@
 // /04_comparison.js
 
-// console.log( 2 > 2 );
-// console.log( 2 >= 2 );
-// console.log( 2 <= 2 );
-// console.log( 2 == 2 );
-// console.log( 2 === 2 );
-// console.log( 2 != 2 );
+console.log( 2 > 2 );
+console.log( 2 >= 2 );
+console.log( 2 <= 2 );
+console.log( 2 == 2 );
+console.log( 2 === 2 );
+console.log( 2 != 2 );
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
 // JS String ko Number ma convert kr deta ha jab Operators k baat ati ha tu 
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);  
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  
 
 // // strict check
 
-// console.log("2" === 2 );
+console.log("2" === 2 );
 
 
