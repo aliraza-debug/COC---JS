66797025171d65cba0697babf034495ebaba3957
Apna Naam <alirazacs007@gmail.com>
2026-04-23 08:11:24 +0500

    WHILE ans DO WHILE

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// ******************* while 
+
+
+let index = 0 
+while (index < 11) {
+    // console.log(`Value of Index is ${index}`)
+    index = index + 3
+}
+
+let arry = [ 2 , 9 , 9 ,33 , 66]
+let arr = 0
+while (arr < arry.length) {
+    console.log(`Value of arry is ${arry[arr]}`);
+    arr++
+}
+
+
+
+// ******************* DO WHILE 
+
+
+let score = 1 
+do {
+    console.log(`score is ${score}`)
+    score ++ 
+} while (score < 11 );
