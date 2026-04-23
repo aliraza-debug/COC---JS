538e4f7136907deb86a53d51a18a222734c859d7
Apna Naam <alirazacs007@gmail.com>
2026-04-23 11:43:16 +0500

    FOR IN

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+//************************ FOR IN  {object k liy use hota ha }
+
+const obj1 = {
+    js : "JAVASCRIPT",
+    ts : "TYPESCRIPT",
+}
+
+for (const key in obj1) {
+    // console.log(`${key} shortcut is for ${obj1[key]}`);
+}
+
+const arr1 = [ 3 , 6 , 9, 6 , 3]
+
+for (const key in arr1) {
+    console.log(arr1[key]);
+}
