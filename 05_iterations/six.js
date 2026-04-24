a2374719e1d03aa1f629352b20efd7b5756f68f4
Apna Naam <alirazacs007@gmail.com>
2026-04-24 12:00:48 +0500

    Filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,13 @@
+const coding = ["JS" , "TS" , "PY" , "CPP"]
+
+const value = coding.forEach((item)=>{
+    // console.log(item);
+    return item ;
+})
+// console.log(value); // for each khuch b return ni krta ha 
+
+const num = [2,6,9,8,3,6,6] 
+const newnum = num.filter( (num)=> { 
+   return num > 4 
+console.log(newnum);
+})
